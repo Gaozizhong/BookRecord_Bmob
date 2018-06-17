0fbd79596fa1d2c03b9fc1ac12b739453a22bdc3
Gaozizhong <631923319@qq.com>
2018-06-17 14:41:13 +0800

    创建工程

---
+++ app/src/main/java/cn/a1949science/www/bookrecord_bomb/StartActivity.java
@@ -0,0 +1,13 @@
+package cn.a1949science.www.bookrecord_bomb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
